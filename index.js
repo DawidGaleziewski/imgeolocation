cb08242f31d6c0cc9eeb58e18188b77d6d7c81e8
Dawid <dawidgalsino@vp.pl>
2019-10-17 22:53:04 +0200

    added basic handling of image to web memory upload

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
+const UploadFileHandler = (function(){
 
-const uploadFileForm = document.querySelector('#upload-file');
-const uploadFileInput = document.querySelector('#upload-file__image');
-const uploadFilePreview = document.querySelector('#upload-preview');
+    function singleImageTemplate(fileObject){
+        //Create DOM elements
+        const pictureInfoList = document.createElement('ul');
+            const imageMiniature = document.createElement('img');
+            const fileName = document.createElement('li');
+            const fileExtension = document.createElement('li');
+            const fileSize = document.createElement('li');
+            const removeButton = document.createElement('li');
 
-const uploadEventHandler = (event)=> {
-    const uploadedFiles = Array.from(event.target.files);
-    const imageElement = document.createElement('img');
+        //Update DOM elements with data
+        imageMiniature.src = URL.createObjectURL(fileObject);
+        fileName.innerText = fileObject.name;
+        fileExtension.innerText = fileObject.type;
+        fileSize.innerText = fileObject.size;
+        removeButton.innerText = 'remove item';
 
-    uploadedFiles.forEach((file, index)=> {
-        imageElement.src = URL.createObjectURL(file);
-        uploadFilePreview.appendChild(imageElement);
-    })
+        //Update the parent list
+        pictureInfoList.appendChild(imageMiniature);
+        pictureInfoList.appendChild(fileName);
+        pictureInfoList.appendChild(fileExtension);
+        pictureInfoList.appendChild(fileSize);
+        pictureInfoList.appendChild(removeButton);
+
+        return pictureInfoList;
+    }
+
+    function imageUploadHandler(fileInput, outputContainer){
+        fileInput.addEventListener('change', function(event){
+            const fileInput = Array.from(event.target.files); 
+            fileInput.forEach(function(file){    
+                outputContainer.appendChild(singleImageTemplate(file));
+            })
+        })
+    }
+    
+    return{
+        UploadImage: imageUploadHandler
+    }
+})()
 
 
-}
+const UIinput = document.querySelector('#upload-file__image');
+const UIoutput = document.querySelector('#upload-preview');
+UploadFileHandler.UploadImage(UIinput, UIoutput);
 
-uploadFileInput.addEventListener('change', uploadEventHandler)
