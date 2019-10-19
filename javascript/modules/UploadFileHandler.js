3778630b24b6ff14bfc6425fb4449e15484c0597
Dawid <dawidgalsino@vp.pl>
2019-10-19 22:17:26 +0200

    finished basic bootstrap look

--- javascript/modules/UploadFileHandler.js
+++ javascript/modules/UploadFileHandler.js
@@ -7,23 +7,32 @@ const UploadFileHandler = (function(ImageGeolocationHandler){
     
     function singleImageTemplate(fileObject){ 
         //Materializecss DOM elements
+        const sizeDiv = document.createElement('div');
+            sizeDiv.classList.add('col');
+            sizeDiv.classList.add('m4');
         const cardDiv = document.createElement('div');
             cardDiv.classList.add('card');
+            // cardDiv.classList.add('col');
         const cardImageDiv = document.createElement('div');
             cardImageDiv.classList.add('card-image');
         const cardTitleSpan = document.createElement('span');
             cardTitleSpan.classList.add('card-title');
         const cardContent = document.createElement('div');
             cardContent.classList.add('card-content');
+        const cardAction = document.createElement('div');
+            cardAction.classList.add('card-action');
 
         //Create DOM elements with data
         const pictureInfoList = document.createElement('ul');
-            // const imageMiniature = document.createElement('li');
-            const image = document.createElement('img');
-            const fileExtension = document.createElement('li');
-            const fileSize = document.createElement('li');
-            let fileGeoLocation = document.createElement('li');
-            const removeButton = document.createElement('li');
+            pictureInfoList.classList.add('collection');
+        const image = document.createElement('img');
+        const fileExtension = document.createElement('li');
+            fileExtension.classList.add('collection-item')
+        const fileSize = document.createElement('li');
+            fileSize.classList.add('collection-item')
+        let fileGeoLocation = document.createElement('li');
+            fileGeoLocation.classList.add('collection-item')
+        const removeButton = document.createElement('li');
 
         //Update DOM elements with data
         image.src = URL.createObjectURL(fileObject);
@@ -31,11 +40,12 @@ const UploadFileHandler = (function(ImageGeolocationHandler){
         fileExtension.innerText = `File extension: ${fileObject.type}`;
         fileSize.innerText = `File size: ${fileObject.size}`;
         removeButton.innerText = 'remove item';
-            removeButton.classList.add('btn-remove')
+            removeButton.classList.add('btn-remove');
+            removeButton.classList.add('btn');
         
         image.onload = function(){
             ImageGeolocationHandler.returnLongLat(image, function(longLat){
-                fileGeoLocation.innerText = `Picture was taken on longitude: ${longLat.longitude}, and latitude: ${longLat.latitude}`
+                fileGeoLocation.innerText = `Picture was taken on long/lat: ${longLat.longitude}, ${longLat.latitude}`;
             })
         }
 
@@ -46,12 +56,14 @@ const UploadFileHandler = (function(ImageGeolocationHandler){
                 pictureInfoList.appendChild(fileExtension);
                 pictureInfoList.appendChild(fileSize);
                 pictureInfoList.appendChild(fileGeoLocation);
-                pictureInfoList.appendChild(removeButton);
+                cardAction.appendChild(removeButton);
             cardContent.appendChild(pictureInfoList);
         cardDiv.appendChild(cardImageDiv)
         cardDiv.appendChild(cardContent)
+        cardDiv.appendChild(cardAction)
+        sizeDiv.appendChild(cardDiv)
 
-        return cardDiv;
+        return sizeDiv;
     }
 
     function imageUploadHandler(fileInput, outputContainer){
@@ -74,7 +86,7 @@ const UploadFileHandler = (function(ImageGeolocationHandler){
 
     function removeImageHandler(imagesContainer){
         imagesContainer.addEventListener('click', function(event){
-            if(event.target.className === "btn-remove"){
+            if(event.target.classList.contains("btn-remove")){
                 event.target.parentElement.parentElement.parentElement.remove()
             }
         })
